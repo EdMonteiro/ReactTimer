4154b16376813c7a5fdd512521efc2c3d074421c
Ed Monteiro <edemirmonteir@gmail.com>
2017-04-14 08:52:32 +0100

    Support foundation settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   entry: [
@@ -22,14 +23,14 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-      Countdown: "app/components/Countdown.jsx",
       Main: 'app/components/Main.jsx',
-      Nav: "app/components/Nav.jsx",
       applicationStyles: 'app/styles/app.scss',
-      Timer: "app/components/Timer.jsx",
-      Clock: "app/components/Clock.jsx",
-      CountdownForm: "app/components/CountdownForm.jsx",
-      Controls: "app/components/Controls.jsx"
+      Nav: 'app/components/Nav.jsx',
+      Timer: 'app/components/Timer.jsx',
+      Countdown: 'app/components/Countdown.jsx',
+      Clock: 'app/components/Clock.jsx',
+      CountdownForm: 'app/components/CountdownForm.jsx',
+      Controls: 'app/components/Controls.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
@@ -43,6 +44,11 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
       }
+    ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
   devtool: 'cheap-module-eval-source-map'
